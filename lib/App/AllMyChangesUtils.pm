8afc07db33614737f698589f38ee60ac5a995961
Ivan Bessarabov <ivan@bessarabov.ru>
2014-08-12 19:34:55 +0000

    Making repo a perl distribution

---
+++ lib/App/AllMyChangesUtils.pm
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+package App::AllMyChangesUtils;
+
+# ABSTRACT: utils for site http://allmychanges.com
+
+=head1 DESCRIPTION
+
+Several scripts that help loading data to the site L< http://allmychanges.com>
+
+=cut
+
+1;
