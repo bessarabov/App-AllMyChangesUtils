a1d028bb29b9a2c3f5f6f2ff826f35e5a852e738
Ivan Bessarabov <ivan@bessarabov.ru>
2015-07-26 10:53:20 +0300

    Using https instead of http link to allmychanges.com

--- lib/App/AllMyChangesUtils.pm
+++ lib/App/AllMyChangesUtils.pm
@@ -2,11 +2,11 @@ use strict;
 use warnings;
 package App::AllMyChangesUtils;
 
-# ABSTRACT: utils for site http://allmychanges.com
+# ABSTRACT: utils for site https://allmychanges.com
 
 =head1 DESCRIPTION
 
-Several scripts that help loading data to the site L<http://allmychanges.com>
+Several scripts that help loading data to the site L<https://allmychanges.com>
 
 =cut
 
